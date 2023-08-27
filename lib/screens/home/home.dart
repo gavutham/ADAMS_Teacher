042fb758c7766607eb42b0f63d73c20be4be53b7
Gavutham <adams4.ifp@gmail.com>
2023-08-27 08:19:42 +0000

    Merged PR 11: create open attendance button
    create open attendance button
    
    Related work items: #7

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,20 @@
 import "package:adams_teacher/models/teacher.dart";
 import "package:adams_teacher/services/auth.dart";
+import "package:adams_teacher/services/database.dart";
 import "package:adams_teacher/shared/loading.dart";
 import "package:flutter/material.dart";
 import "package:provider/provider.dart";
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final _auth = AuthService();
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -23,10 +31,27 @@ class Home extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text("Welcome to ADAMS - Teacher, ${teacher.name}", style: const TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.w900,
-        ),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text("Welcome, ${teacher.name}", style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w900,
+            ),),
+            ElevatedButton(
+              onPressed: loading ? null : () async {
+                setState(() {
+                  loading = true;
+                });
+                await DatabaseService(tid: teacher.tid).openAttendance(teacher.classes, teacher.tid);
+                setState(() {
+                  loading = false;
+                });
+              },
+              child: const Text("Open Attendance"),
+            ),
+          ],
+        ),
       ) ,
     ) : const Loading();
   }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,7 @@
 import 'package:adams_teacher/models/teacher.dart';
+import 'package:adams_teacher/utils/datetime.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class DatabaseService {
   String? tid;
@@ -7,6 +9,7 @@ class DatabaseService {
   DatabaseService({required this.tid});
 
   final CollectionReference teacherCollection = FirebaseFirestore.instance.collection("teachers");
+  final CollectionReference timetableCollection = FirebaseFirestore.instance.collection("timetable");
 
   TeacherData _teacherDataFromFirebase(Map data) {
     return TeacherData(
@@ -34,5 +37,38 @@ class DatabaseService {
     };
 
     return await teacherRef.set(teacherData);
+  }
+
+  Future openAttendance(List classes, String tid) async {
+
+    final currentInterval = getCurrentInterval();
+    final currentDay = getFormattedDay();
+
+    try {
+      for (var element in classes) {
+        final [year, dept, sec] = element.split("-");
+        final data = await timetableCollection
+            .doc(year)
+            .collection(dept)
+            .doc(sec)
+            .collection(currentDay)
+            .doc(currentInterval)
+            .get()
+            .then((value) => value.data() as Map<String, dynamic>);
+
+        if (data["tid"] == tid) {
+          final DatabaseReference realtimeDb = FirebaseDatabase.instance.ref("/$year/$dept");
+          await realtimeDb.update({
+            sec: true,
+          });
+          return true;
+        }
+      }
+      return null;
+    }catch (err) {
+      print("error");
+      print(err);
+      return null;
+    }
   }
 }
