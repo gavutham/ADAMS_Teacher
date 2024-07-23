6426d6efbf00971600b87160cc02f0f55b084185
Johann Sylvester J <johannsylvester04@gmail.com>
2024-07-23 17:03:52 +0530

    Handle beacon ips and bb verify data.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -30,7 +30,7 @@ class _HomeState extends State<Home> {
   static const sections = ["A", "B", "C"];
   static const times = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-  DropdownMenuItem<String> toDropDownItem (value) {
+  DropdownMenuItem<String> toDropDownItem(value) {
     return DropdownMenuItem<String>(
       value: value,
       child: Text(value),
@@ -52,7 +52,8 @@ class _HomeState extends State<Home> {
         var _sec;
 
         if (!manual) {
-          (_year, _dept, _sec) = (await (DatabaseService(tid: teacher.tid)).openAttendance(teacher.classes, teacher.tid));
+          (_year, _dept, _sec) = (await (DatabaseService(tid: teacher.tid))
+              .openAttendance(teacher.classes, teacher.tid));
         } else {
           (_year, _dept, _sec) = (year, dept, sec);
           print("manual");
@@ -63,7 +64,10 @@ class _HomeState extends State<Home> {
         turnOn();
         var nearby = await getDevices();
 
-        await postNearbyDevices(nearby, {"year":_year, "dept":_dept, "sec":_sec});
+        await postNearbyBeaconScanDetails(
+            {"year": _year, "dept": _dept, "sec": _sec});
+        await postNearbyDevices(
+            nearby, {"year": _year, "dept": _dept, "sec": _sec});
 
         setState(() {
           loading = false;
@@ -71,122 +75,131 @@ class _HomeState extends State<Home> {
       }
     }
 
-    return teacher != null ?  Scaffold(
-      appBar: AppBar(
-        title: const Text("ADAMS"),
-        actions: [
-          ElevatedButton(
-            onPressed: () {_auth.signOut();},
-            child: const Text("Logout"),
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text("Welcome, ${teacher.name}", style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w900,
-            ),),
-            ElevatedButton(
-              onPressed: loading ? null : handleSubmit,
-              child: const Text("Open Attendance"),
+    return teacher != null
+        ? Scaffold(
+            appBar: AppBar(
+              title: const Text("ADAMS"),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    _auth.signOut();
+                  },
+                  child: const Text("Logout"),
+                )
+              ],
             ),
-            CupertinoSwitch(
-              value: manual,
-              onChanged: (value) {
-                setState(() {
-                  manual = value;
-                  print(manual);
-                });
-              },
-            ),
-            AbsorbPointer(
-              absorbing: !manual,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 20),
-                child: Wrap(
-                  spacing: 10,
-                  children: [
-                    DropdownButton(
-                      items: years.map((e) => toDropDownItem(e)).toList(),
-                      onChanged: (val) {
-                        if(val != null) {
-                          year = val;
-                          print(val);
-                        }
-                      },
-                      hint: Container(
-                        width: 100,
-                        child: Text(
-                          year ?? "Year",
-                          style: const TextStyle(color: Colors.grey),
-                          textAlign: TextAlign.end,
-                        ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text(
+                    "Welcome, ${teacher.name}",
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: loading ? null : handleSubmit,
+                    child: const Text("Open Attendance"),
+                  ),
+                  CupertinoSwitch(
+                    value: manual,
+                    onChanged: (value) {
+                      setState(() {
+                        manual = value;
+                        print(manual);
+                      });
+                    },
+                  ),
+                  AbsorbPointer(
+                    absorbing: !manual,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 0, horizontal: 20),
+                      child: Wrap(
+                        spacing: 10,
+                        children: [
+                          DropdownButton(
+                            items: years.map((e) => toDropDownItem(e)).toList(),
+                            onChanged: (val) {
+                              if (val != null) {
+                                year = val;
+                                print(val);
+                              }
+                            },
+                            hint: Container(
+                              width: 100,
+                              child: Text(
+                                year ?? "Year",
+                                style: const TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ),
+                          DropdownButton(
+                            value: dept,
+                            items: depts.map((e) => toDropDownItem(e)).toList(),
+                            onChanged: (val) {
+                              if (val != null) {
+                                dept = val;
+                                print(val);
+                              }
+                            },
+                            hint: Container(
+                              width: 100,
+                              child: Text(
+                                dept ?? "Dept",
+                                style: const TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ),
+                          DropdownButton(
+                            value: sec,
+                            items:
+                                sections.map((e) => toDropDownItem(e)).toList(),
+                            onChanged: (val) {
+                              if (val != null) {
+                                sec = val;
+                                print(val);
+                              }
+                            },
+                            hint: Container(
+                              width: 100,
+                              child: Text(
+                                sec ?? "Sec",
+                                style: const TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ),
+                          DropdownButton(
+                            value: time,
+                            items: times.map((e) => toDropDownItem(e)).toList(),
+                            onChanged: (val) {
+                              if (val != null) {
+                                time = val;
+                                print(val);
+                              }
+                            },
+                            hint: Container(
+                              width: 100,
+                              child: Text(
+                                time ?? "Time",
+                                style: const TextStyle(color: Colors.grey),
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    DropdownButton(
-                      value: dept,
-                      items: depts.map((e) => toDropDownItem(e)).toList(),
-                      onChanged: (val) {
-                        if(val != null) {
-                          dept = val;
-                          print(val);
-                        }
-                      },
-                      hint: Container(
-                        width: 100,
-                        child: Text(
-                          dept ?? "Dept",
-                          style: const TextStyle(color: Colors.grey),
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ),
-                    DropdownButton(
-                      value: sec,
-                      items: sections.map((e) => toDropDownItem(e)).toList(),
-                      onChanged: (val) {
-                        if(val != null) {
-                          sec = val;
-                          print(val);
-                        }
-                      },
-                      hint: Container(
-                        width: 100,
-                        child: Text(
-                          sec ?? "Sec",
-                          style: const TextStyle(color: Colors.grey),
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ),
-                    DropdownButton(
-                      value: time,
-                      items: times.map((e) => toDropDownItem(e)).toList(),
-                      onChanged: (val) {
-                        if(val != null) {
-                          time = val;
-                          print(val);
-                        }
-                      },
-                      hint: Container(
-                        width: 100,
-                        child: Text(
-                          time ?? "Time",
-                          style: const TextStyle(color: Colors.grey),
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ) ,
-    ) : const Loading();
+            ),
+          )
+        : const Loading();
   }
 }
--- lib/services/server.dart
+++ lib/services/server.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-const baseUrl = "http://144.91.106.164:8000";
-
+const baseUrl = "http://192.168.71.129:8080";
 
 Future startSession(year, dept, sec) async {
   var base = "$baseUrl/start-session";
@@ -13,11 +12,9 @@ Future startSession(year, dept, sec) async {
   await http.get(Uri.parse(url));
 }
 
-
 Future postNearbyDevices(List<Map> nearby, Map class_) async {
   var base = "$baseUrl/pp-verify";
-  var url =
-      '$base/${class_["year"]}/${class_["dept"]}/${class_["sec"]}';
+  var url = '$base/${class_["year"]}/${class_["dept"]}/${class_["sec"]}';
 
   print("Url");
   print(url);
@@ -25,6 +22,34 @@ Future postNearbyDevices(List<Map> nearby, Map class_) async {
   print(nearby);
 
   await http.post(Uri.parse(url),
-      headers: {"Content-type": "application/json"},
-      body: jsonEncode(nearby));
+      headers: {"Content-type": "application/json"}, body: jsonEncode(nearby));
+}
+
+Future<dynamic> getBeaconScan(String ip) async {
+  var url = "http://$ip/ble_scan";
+  var response = await http.get(Uri.parse(url)).timeout(
+    const Duration(seconds: 20),
+    onTimeout: () {
+      // Time has run out, do what you wanted to do.
+      return http.Response(
+          'Error', 408); // Request Timeout response status code
+    },
+  );
+  return jsonDecode(response.body);
+}
+
+Future postNearbyBeaconScanDetails(Map class_) async {
+  var url =
+      "$baseUrl/get-beacon-ips/${class_["year"]}/${class_["dept"]}/${class_["sec"]}";
+  var response = await http.get(Uri.parse(url));
+  List<dynamic> nearbyBeaconIPs = jsonDecode(response.body);
+  // var outputScan = await getBeaconScan(nearbyBeaconIPs[0]);
+  // return outputScan;
+  var nearbyBeaconScanDetails =
+      await Future.wait(nearbyBeaconIPs.map((ip) => getBeaconScan(ip)));
+  url =
+      "$baseUrl/bb-verify/${class_["year"]}/${class_["dept"]}/${class_["sec"]}";
+  await http.post(Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(nearbyBeaconScanDetails));
 }
